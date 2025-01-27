55b16f5f0b0749b706e4a983248ac3d8c3af7525
Pann Phyu <pannphyu24497@gmail.com>
2025-01-27 11:31:14 +0630

    To fix some issues

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,12 @@
 from application import app
 from flask import render_template
-
+from .forms import TodoForm
 
 @app.route("/")
 def index():
     return render_template("todos_view.html", title="Layout Page")
+
+@app.route("/add_todo")
+def add_todo():
+    form = TodoForm()
+    return render_template("add_todo.html", form = form)
