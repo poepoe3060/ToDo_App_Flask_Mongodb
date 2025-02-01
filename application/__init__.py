b97e3a571397d16ab44595b74dc2d2745584145e
Pann Phyu <pannphyu24497@gmail.com>
2025-02-01 15:49:16 +0630

    To modify routes and mongo setup

--- application/__init__.py
+++ application/__init__.py
@@ -3,11 +3,16 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "56dbb76de41e1376094d3b05fd35cde93909f4ea"
-app.config["MONGO_URI"] = "mongodb+srv://root:root@cluster0.u3ckb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+app.config["MONGO_URI"] = "mongodb+srv://poepoe:poepoe@cluster0.u3ckb.mongodb.net/todo_flask?retryWrites=true&w=majority"
 
-
-# setup mongodb
 mongodb_client = PyMongo(app)
-db = mongodb_client.db
+db = mongodb_client.db  
+
+try:
+    print("Connected to MongoDB successfully!")
+    print("Existing collections:", db.list_collection_names())  
+except Exception as e:
+    print("MongoDB Connection Error:", e)
+
 
 from application import routes  
