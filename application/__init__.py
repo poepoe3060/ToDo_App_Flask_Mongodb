fc8f4b2d54477b81c07d6e90b178385bac8718ef
Loon Poe Phyu <loonpoephyu@Loons-MacBook-Air.local>
2025-02-06 23:06:29 +0630

    fix runtime error on mac

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,4 @@
+import certifi
 from flask import Flask
 from flask_pymongo import PyMongo
 
@@ -5,8 +6,8 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "56dbb76de41e1376094d3b05fd35cde93909f4ea"
 app.config["MONGO_URI"] = "mongodb+srv://poepoe:poepoe@cluster0.u3ckb.mongodb.net/todo_flask?retryWrites=true&w=majority"
 
-mongodb_client = PyMongo(app)
+# Pass certifi's CA file
+mongodb_client = PyMongo(app, tlsCAFile=certifi.where())
 db = mongodb_client.db  
 
-
-from application import routes  
+from application import routes
