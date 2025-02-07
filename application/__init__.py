48778c09ad7b0f641294fa3db235f68e66f4c186
Pann Phyu <pannphyu24497@gmail.com>
2025-02-07 15:04:15 +0630

    fix bug

--- application/__init__.py
+++ application/__init__.py
@@ -3,10 +3,10 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "56dbb76de41e1376094d3b05fd35cde93909f4ea"
-app.config["MONGO_URI"] = "mongodb+srv://poepoe:poepoe@cluster0.u3ckb.mongodb.net/todo_flask?retryWrites=true&w=majority"
+app.config["SECRET_KEY"] = "56dbb76de4113456789e1376094d3b05fd35cde93909f4ea"
+app.config["MONGO_URI"] = "mongodb+srv://poepoe:****@cluster0.u3ckb.mongodb.net/todo_flask?retryWrites=true&w=majority"
 
-# Pass certifi's CA file
+
 mongodb_client = PyMongo(app, tlsCAFile=certifi.where())
 db = mongodb_client.db  
 
